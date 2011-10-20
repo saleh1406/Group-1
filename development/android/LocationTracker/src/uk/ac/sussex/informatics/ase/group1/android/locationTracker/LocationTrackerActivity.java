c6b55c0c3841a35f3d9eefab690ffe955184d0d4
Andy Keavey <a.a.keavey@sussex.ac.uk>
2011-10-20 14:07:44 +0100

    First commit of LocationTracker v0.01 android project to dev branch, implements very basic prototype application for Work Order 002 (task 2)

---
+++ development/android/LocationTracker/src/uk/ac/sussex/informatics/ase/group1/android/locationTracker/LocationTrackerActivity.java
@@ -0,0 +1,113 @@
+package uk.ac.sussex.informatics.ase.group1.android.locationTracker;
+
+import java.text.DateFormat;
+
+import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Prints the users location, latitude and longitude, as well as the GPS time in UTC format in a simple
+ * linear layout activity.
+ * 
+ * @author Andy Keavey
+ * @version 0.01
+ *
+ */
+public class LocationTrackerActivity extends Activity {
+	
+	private LocationListener ll;
+	private LocationManager lm;
+	
+	private TextView latitude, longitude, gpsTime;
+	private CharSequence latitudeText, longitudeText;
+	private DateFormat dateFormat = DateFormat.getInstance();
+	
+    /**
+     * Find the text views and use a location manager to get location updates 
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        /*
+         * To support multiple languages, we get the string set by the String resource file.
+         */
+        latitude = (TextView) findViewById(R.id.user_latitude);
+        longitude = (TextView) findViewById(R.id.user_longitude); 
+        gpsTime = (TextView) findViewById(R.id.gps_time);       
+        latitudeText = latitude.getText();
+        longitudeText = longitude.getText();
+        
+	    lm = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);    
+	    ll = new GPSLocationListener(); 
+		lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+    }
+    
+    @Override
+    protected void onStart() {
+    	super.onStart();
+    	//do nothing
+    }
+    
+    @Override
+    protected void onPause() {
+    	super.onPause();
+    	//lm.removeUpdates(ll);
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, ll);
+    }
+    
+    /**
+     * Remove location updates to save battery power when application not active.
+     */
+    @Override
+    protected void onStop() {
+    	super.onStop();
+    	lm.removeUpdates(ll);
+    }
+    
+    /*
+     * The setText method must be called from within the Activity class containing the target TextView
+     * This code could be put in a handler.
+     * We use the GPS time (UTC) as it will be standard in all locations, system time is displayed on the
+     * phone's status bar anyway.
+     */
+    private void showLocation(Location location) {    
+        latitude.setText(latitudeText + " " + location.getLatitude());
+        longitude.setText(longitudeText + " " + location.getLongitude());
+        gpsTime.setText("UTC: " + dateFormat.format(location.getTime()));
+    }
+    
+    /**
+     * Inner class responsible for getting information about the users location.
+     * @author Andy
+     *
+     */
+    class GPSLocationListener implements LocationListener {
+    	public void onLocationChanged(Location location) {
+    		showLocation(location);
+    	}
+    	
+    	public void onStatusChanged(String provider, int status, Bundle extras) {
+    		
+    	}
+    	
+    	public void onProviderEnabled(String provider) {
+    		
+    	}
+    	
+    	public void onProviderDisabled(String provider) {
+    		
+    	}
+    } //end inner class GPSLocationListener
+}
